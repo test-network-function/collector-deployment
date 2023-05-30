332c6532d23433aeb3ab60ae44f6afd40339de3f
Shir Moran <shmoran@shmoran.tlv.csb>
2023-05-30 18:46:20 +0300

    changes user's name, granted min privileges

--- database/create_user.sql
+++ database/create_user.sql
@@ -1,3 +1,4 @@
-CREATE USER 'sqluser'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
-GRANT ALL PRIVILEGES ON *.* TO 'sqluser'@'%';
+CREATE USER 'collectoruser'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
+GRANT ALL PRIVILEGES ON cnf.claim TO 'collectoruser'@'%';
+GRANT ALL PRIVILEGES ON cnf.claim_result TO 'collectoruser'@'%';
 FLUSH PRIVILEGES;
