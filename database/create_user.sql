b952d9032fcfd09a4a737fc34e760d2dc18ced26
Shir Moran <shmoran@shmoran.tlv.csb>
2023-06-21 12:26:15 +0300

    Added db scripts execution on start time

--- database/create_user.sql
+++ database/create_user.sql
@@ -1,4 +1,4 @@
-CREATE USER 'collectoruser'@'%' IDENTIFIED BY 'password';
+CREATE USER if not exists 'collectoruser'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON cnf.claim TO 'collectoruser'@'%';
 GRANT ALL PRIVILEGES ON cnf.claim_result TO 'collectoruser'@'%';
 FLUSH PRIVILEGES;
